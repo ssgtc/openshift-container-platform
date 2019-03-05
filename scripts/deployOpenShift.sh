ac52228c74d987ecf692e1f383c49704da76465a
haroldwongms <harold.wong@microsoft.com>
2019-03-04 20:08:17 -0700

    add vars for CNS

--- scripts/deployOpenShift.sh
+++ scripts/deployOpenShift.sh
@@ -230,7 +230,12 @@ if [[ $ENABLECNS == "true" ]]
 then
     echo $(date) " - Creating glusterfs configuration"
 
-	STORAGECLASS="openshift_storage_glusterfs_storageclass_default=true
+	STORAGECLASS="# Specify CNS images and storage class defaults
+openshift_storage_glusterfs_image=registry.access.redhat.com/rhgs3/rhgs-server-rhel7:v3.11
+openshift_storage_glusterfs_block_image=registry.access.redhat.com/rhgs3/rhgs-gluster-block-prov-rhel7:v3.11
+openshift_storage_glusterfs_s3_image=registry.access.redhat.com/rhgs3/rhgs-s3-server-rhel7:v3.11
+openshift_storage_glusterfs_heketi_image=registry.access.redhat.com/rhgs3/rhgs-volmanager-rhel7:v3.11
+openshift_storage_glusterfs_storageclass_default=true
 openshift_storageclass_default=false"
 
 	# Ensuring selinux is configured properly
